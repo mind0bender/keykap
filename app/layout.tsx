e6114d36119180c7016df7ab74e19cc60197a166
mind0bender <mind0bender@gmail.com>
2024-08-09 23:30:31 +0530

    { "C": ["home page basic"] }

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-orange-100 text-orange-900`}>
+        {children}
+      </body>
     </html>
   );
 }
